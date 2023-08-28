73c4af94756d07d31bf3795ae587cd2f00fbe16b
Nick <nicholasklvanahooper@gmail.com>
2023-08-28 23:48:08 +1000

    Kinda adds to db.csv

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import *
-app = Flask(__name__)
 import os
 import pandas as pd
 from werkzeug.utils import secure_filename
+from datetime import datetime
 
+app = Flask(__name__)
 UPLOAD_FOLDER = os.path.join('/var/www/uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -11,7 +12,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = {'csv'}
 
 @app.route('/')
-def hello_geek():
+def home_page():
     return "hello world"
 
 @app.route('/upload', methods=['POST'])
@@ -22,9 +23,33 @@ def uploadFile():
 
         # Extract a "safe" version of the filepath
         data_filename = secure_filename(f.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'],
+                            data_filename)
+        
+        f.save(filepath)
+        
+        # Open the file in a pandas data frame
+        new_df = pd.read_csv(filepath,
+                         encoding='unicode_escape')
+        
+        new_df['date']= pd.to_datetime(new_df['date'], dayfirst=True)
+        
+        df = pd.read_csv("/var/www/db/db.csv")
+        df['date'] = pd.to_datetime(df['date'])
 
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'],
-                            data_filename))
+        # Sort the data frame by date
+        new_df.sort_values("date")
+
+        # Check if the earliest is later than the latest in pre-existing DF
+        # (Easy check to avoid collisions for now...)
+        if new_df["date"].iloc[0] > df["date"].iloc[-1]:
+            new = pd.concat([df, new_df], axis=0, ignore_index=True)
+            
+
+        # Then if so, we can add the data frame to the current one
+        # Export the dataframe as a CSV to /var/www/db/db.csv
+        new.to_csv("/var/www/db/db.csv")
+
     return "success!"
 
 
