af3f585606ec5296513a653c9fdf8d5b51cc7652
Nick <nicholasklvanahooper@gmail.com>
2024-03-28 17:55:47 +1100

    Added a lot...

--- flutter_flasking_around/lib/screens/home.dart
+++ flutter_flasking_around/lib/screens/home.dart
@@ -1,4 +1,5 @@
-import 'package:fluent_ui/fluent_ui.dart';
+// import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart';
 import 'package:url_launcher/link.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,58 +15,11 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    assert(debugCheckHasFluentTheme(context));
+    // assert(debugCheckHasFluentTheme(context));
     // final theme = FluentTheme.of(context);
 
-    return ScaffoldPage.scrollable(
-      header: PageHeader(
-        title: const Text('Home Screen'),
-        commandBar: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-          Link(
-            uri: Uri.parse('https://github.com/bdlukaa/fluent_ui'),
-            builder: (context, open) => Semantics(
-              link: true,
-              child: Tooltip(
-                message: 'Source code',
-                child: IconButton(
-                  icon: const Icon(FluentIcons.open_source, size: 24.0),
-                  onPressed: open,
-                ),
-              ),
-            ),
-          ),
-        ]),
-      ),
-      children: const [
-      ],
+    return Column(
+      children: [Text("HI")],
     );
-  }
-}
-
-class SponsorButton extends StatelessWidget {
-  const SponsorButton({
-    super.key,
-    required this.imageUrl,
-    required this.username,
-  });
-
-  final String imageUrl;
-  final String username;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        height: 60,
-        width: 60,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(imageUrl),
-          ),
-          shape: BoxShape.circle,
-        ),
-      ),
-      Text(username),
-    ]);
   }
 }
