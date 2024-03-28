bca47621df9dc1968881347d66085758e000c2c7
Nick <nicholasklvanahooper@gmail.com>
2024-03-28 22:43:40 +1100

    account apis work now

--- models.py
+++ models.py
@@ -1,14 +1,32 @@
 from config import db, ma
+from marshmallow_sqlalchemy import fields
 
 
 class Transaction(db.Model):
     __tablename__ = "transactions"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    account = db.Column(db.Integer, db.ForeignKey("accounts.id"))
     date = db.Column(db.String(32))
     desc = db.Column(db.String(255))
     cost = db.Column(db.Float)
     balance = db.Column(db.Float)
+
+
+class Account(db.Model):
+    __tablename__ = "accounts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nickname = db.Column(db.String(32))
+    bank_name = db.Column(db.String(32))
+    account_type = db.Column(db.String(32))
+    rel_transactions = db.relationship(
+        Transaction,
+        backref="accounts",
+        cascade="all, delete, delete-orphan",
+        single_parent=True,
+        order_by="desc(Transaction.date)",
+    )
 
 
 class TransactionSchema(ma.SQLAlchemyAutoSchema):
@@ -16,7 +34,20 @@ class TransactionSchema(ma.SQLAlchemyAutoSchema):
         model = Transaction
         load_instance = True
         sqla_session = db.session
+        include_relationships = True
+
+
+class AccountSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Account
+        load_instance = True
+        sqla_session = db.session
+        include_fk = True
+
+    transactions = fields.Nested(TransactionSchema, many=True)
 
 
 transaction_schema = TransactionSchema()
 transactions_schema = TransactionSchema(many=True)
+account_schema = AccountSchema()
+accounts_schema = AccountSchema(many=True)
