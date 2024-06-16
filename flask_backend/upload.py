419c2bf55d6c8c48ba8f7925a640a29ddf561cae
Nick <nicholasklvanahooper@gmail.com>
2024-06-16 22:32:59 +0800

    Added filtering system backend

--- flask_backend/upload.py
+++ flask_backend/upload.py
@@ -1,6 +1,13 @@
 from config import db
 from flask import abort, make_response
-from models import Transaction, transaction_schema, transactions_schema, Account
+from models import (
+    Transaction,
+    transaction_schema,
+    transactions_schema,
+    Account,
+    Filter,
+    filters_schema,
+)
 from accounts import read_one
 
 from io import BytesIO
@@ -15,7 +22,6 @@ def upload_file(account_id_query, file):
     # convert from byte string to the dataframe
     col_names = ["date", "cost", "desc", "balance"]
     df = pd.read_csv(BytesIO(file), names=col_names, header=None)
-    print(account_id_query, df)
     df["balance"].fillna("0.00", inplace=True)
 
     # naive front and back check to see that the transaction ranges don't already
@@ -46,28 +52,44 @@ def upload_file(account_id_query, file):
         Transaction.balance == balance,
     ).one_or_none()
 
-    # Since it is a dataframe we can apply regex and search for things
-    # Maybe match against a database that we keep - that we can edit in the UI
-    # Look for "MCdonalds" and then give it the food category or something
-    # Maybe if ABNs or something we can implement a search if it hasn't been seen before
-    # Use some API or whatever
-    if first_transaction_from_csv is None and last_transaction_from_csv is None:
-        print("HI Nick")
-        for i, row in df.iterrows():
-            print(row["balance"], row["cost"])
-            new_transaction = {
-                "accounts": account_id_query,
-                "date": row["date"],
-                "desc": row["desc"],
-                "cost": row["cost"],
-                "balance": row["balance"],
-            }
+    filters = Filter.query.all()
+    filters = filters_schema.dump(filters)
+    print(filters)
 
-            new_transaction = transaction_schema.load(
-                new_transaction, session=db.session
-            )
-            db.session.add(new_transaction)
-            db.session.commit()
-        return transaction_schema.dump(new_transaction), 201
+    if filters is None:
+        abort(406, f"No filters found")
     else:
-        abort(406, f"Duplicate transaction found - aborting")
+        df["category"] = ""
+
+        for filter in filters:
+            df["category"] = df["category"].mask(
+                df["desc"].str.contains(filter["searchterm"]), filter["category"]
+            )
+
+        # Since it is a dataframe we can apply regex and search for things
+        # Maybe match against a database that we keep - that we can edit in the UI
+        # Look for "MCdonalds" and then give it the food category or something
+        # Maybe if ABNs or something we can implement a search if it hasn't been seen before
+        # Use some API or whatever
+        if first_transaction_from_csv is None and last_transaction_from_csv is None:
+            for i, row in df.iterrows():
+                new_transaction = {
+                    "accounts": account_id_query,
+                    "date": row["date"],
+                    "desc": row["desc"],
+                    "cost": row["cost"],
+                    "balance": row["balance"],
+                }
+
+                # If it has a category
+                if row["category"] != "":
+                    new_transaction["category"] = row["category"]
+
+                new_transaction = transaction_schema.load(
+                    new_transaction, session=db.session
+                )
+                db.session.add(new_transaction)
+                db.session.commit()
+            return transaction_schema.dump(new_transaction), 201
+        else:
+            abort(406, f"Duplicate transaction found - aborting")
